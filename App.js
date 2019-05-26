f999c2424f261f0605d1d708a33f0cbefda9a487
Raúl Contreras <raulc98@gmail.com>
2019-05-26 13:15:07 +0200

    Created ListItem component

--- App.js
+++ App.js
@@ -7,11 +7,13 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, TextInput} from 'react-native';
+import {StyleSheet, Button, View, TextInput, Text} from 'react-native';
+import ListItem from './src/components/ListItem/ListItem';
 
 export default class App extends Component {
   state = {
-    placeName: ''
+    placeName: '',
+    places: []
   }
 
   placeNameChangedHandler = (val) => {
@@ -19,11 +21,30 @@ export default class App extends Component {
       placeName: val
     })
   }
+
+  placeSubmitHandler = () => {
+    if(this.state.placeName.trim() === '') return;
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName),
+        placeName: ''
+      };
+    });
+  }
   
   render() {
+    const placesOutput = this.state.places.map((place, index) => <ListItem key={index} placeName={place}/>)
+
     return (
       <View style={styles.container}>
-        <TextInput style={{flex:1}} placeholder='An awesome page' value={this.state.placeName} onChangeText={this.placeNameChangedHandler}/>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.input} placeholder='An awesome place' value={this.state.placeName} onChangeText={this.placeNameChangedHandler}/>
+          <Button title="Add" style={styles.button} onPress={this.placeSubmitHandler} />
+        </View>
+        <View style={styles.listView}>
+          {placesOutput}
+        </View>
       </View>
     );
   }
@@ -31,11 +52,25 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop:20,
-    flex: 1,
-    flexDirection: 'row',
+    marginTop:20,
     justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  inputContainer: {
+    width: '100%',
+    padding:20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '70%'
+  },
+  button: {
+    width: '30%'
+  },
+  listView: {
+    width: '100%'
   }
 });
