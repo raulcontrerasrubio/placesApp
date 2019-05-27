5d6324ec79a6d6812169ca9f96855d5a1baef68f
Raúl Contreras <raulc98@gmail.com>
2019-05-27 10:34:03 +0200

    Added icons to tab navigation

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,20 +1,32 @@
 import {Navigation} from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
 
 const startTabs = () => {
-  Navigation.startTabBasedApp({
-    tabs: [
-      {
-        screen: 'placesApp.FindPlaceScreen',
-        label: 'Find Place',
-        title: 'Find Place'
-      },
-      {
-        screen: 'placesApp.SharePlaceScreen',
-        label: 'Share Place',
-        title: 'Share Place'
-      },
-    ]
-  });
+  const map = Icon.getImageSource('md-map', 30);
+  const share = Icon.getImageSource('ios-share-alt', 30);
+
+  Promise.all([map, share])
+    .then(icons => {
+      Navigation.startTabBasedApp({
+        tabs: [
+          {
+            screen: 'placesApp.FindPlaceScreen',
+            label: 'Find Place',
+            title: 'Find Place',
+            icon: icons[0]
+          },
+          {
+            screen: 'placesApp.SharePlaceScreen',
+            label: 'Share Place',
+            title: 'Share Place',
+            icon: icons[1]
+          },
+        ]
+      });
+    })
+
 };
 
 export default startTabs;
